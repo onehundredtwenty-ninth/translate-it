76a4825013d47b3ae78a76af13c7402b57182836
Alexander <a.kireev.qa@gmail.com>
2023-04-05 21:06:11 +0300

    feat: добавить сгенерированное spring initializr

---
+++ src/main/java/ru/translator/translateit/TranslateItApplication.java
@@ -0,0 +1,12 @@
+package ru.translator.translateit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateItApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/translator/translateit/TranslateItApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.translator.translateit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
