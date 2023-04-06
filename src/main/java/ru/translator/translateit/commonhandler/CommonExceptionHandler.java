e53fa330e1f01d38e87806eeaeddbdd230a2e296
Alexander <a.kireev.qa@gmail.com>
2023-04-07 02:00:43 +0300

    feat: добавить обработку MethodArgumentNotValidException

--- src/main/java/ru/translator/translateit/commonhandler/CommonExceptionHandler.java
+++ src/main/java/ru/translator/translateit/commonhandler/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.translator.translateit.commonhandler;
 
 import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +13,12 @@ public class CommonExceptionHandler {
   @ExceptionHandler
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponse handleIllegalArgumentException(final IllegalArgumentException e) {
+    return new ErrorResponse(HttpStatus.BAD_REQUEST, "Incorrectly made request.", e.getMessage(), LocalDateTime.now());
+  }
+
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
     return new ErrorResponse(HttpStatus.BAD_REQUEST, "Incorrectly made request.", e.getMessage(), LocalDateTime.now());
   }
 }
