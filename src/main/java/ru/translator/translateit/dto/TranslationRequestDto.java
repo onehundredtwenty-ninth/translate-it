090a7c707994916926b7b13e5a6426d4c320b8c5
Alexander <a.kireev.qa@gmail.com>
2023-04-06 19:37:17 +0300

    refactor: добавить конструктор со всеми параметрами

--- src/main/java/ru/translator/translateit/dto/TranslationRequestDto.java
+++ src/main/java/ru/translator/translateit/dto/TranslationRequestDto.java
@@ -2,11 +2,15 @@ package ru.translator.translateit.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TranslationRequestDto {
 
   @NotBlank
